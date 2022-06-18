bb4dbc723292e2bdb4622ff891f64d1f92fea6a3
Ohwaaan <owenlian88@gmail.com>
2022-06-18 15:30:50 -0400

    finished addR and addC

--- script.js
+++ script.js
@@ -1,16 +1,41 @@
 // Declare global variables
 let numRows = 0;
 let numCols = 0;
+let rowArr = [];
 let colorSelected; 
+let grid = document.getElementById("grid");
 
 // Add a row
 function addR() {
-    alert("Clicked Add Row"); // Replace this line with your code.
+
+    if (numRows == 0 && numCols == 0){  //incase empty
+        rowArr[0] = grid.insertRow(0);
+        rowArr[0].insertCell(0);
+        numCols++;
+        numRows++;
+    } else {                            //Add a row with the amount of columns  
+        rowArr[numRows] = grid.insertRow(0);
+        for(i=0; i<numCols; i++) {
+            rowArr[numRows].insertCell(0);      
+        }
+        numRows++;
+    }
 }
 
 // Add a column
 function addC() {
-    alert("Clicked Add Col"); // Replace this line with your code.
+
+    if (numRows == 0 && numCols == 0){  //incase empty
+        rowArr[0] = grid.insertRow(0);
+        rowArr[0].insertCell(0);
+        numRows++;
+        numCols++;
+    } else {                            //add column with amount of rows
+        for(i=0; i< numRows; i++) {
+            rowArr[i].insertCell(0);
+        }
+        numCols++
+    }
 }
 
 // Remove a row
@@ -21,6 +46,7 @@ function removeR() {
 // Remove a column
 function removeC() {
     alert("Clicked Remove Col"); // Replace this line with your code.
+
 }
 
 // Set global variable for selected color
